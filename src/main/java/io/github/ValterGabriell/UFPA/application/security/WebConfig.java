7fee6189c747033439d558e93835c600abdc8cd3
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-04-11 18:06:53 -0300

    adicionando funcionalidade para enviar imagem para o imgur

---
+++ src/main/java/io/github/ValterGabriell/UFPA/application/security/WebConfig.java
@@ -0,0 +1,35 @@
+package io.github.ValterGabriell.UFPA.application.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.cors().and().csrf().disable().build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "PUT", "POST", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
